3e3638b7fab18c96314c007edcc9cf88cac3d455
goliatone <140582+goliatone@users.noreply.github.com>
2026-05-10 14:59:30 -0700

    add: command handler for semantic diff

--- commands/webcap/semantic_diff.go
+++ commands/webcap/semantic_diff.go
@@ -2,7 +2,6 @@ package webcap
 
 import (
 	"context"
-	"fmt"
 
 	command "github.com/goliatone/go-command"
 	pkgwebcap "github.com/goliatone/webcap"
@@ -15,23 +14,17 @@ type SemanticDiffMessage struct {
 func (SemanticDiffMessage) Type() string { return "webcap::semantic_diff" }
 
 type SemanticDiffHandler struct {
-	Service pkgwebcap.SemanticDiffService
+	*commandHandler[SemanticDiffMessage, pkgwebcap.SemanticDiffResult]
 }
 
 func NewSemanticDiffHandler(service pkgwebcap.SemanticDiffService) *SemanticDiffHandler {
-	return &SemanticDiffHandler{Service: service}
-}
-
-func (h *SemanticDiffHandler) Execute(ctx context.Context, msg SemanticDiffMessage) error {
-	_, err := h.Handle(ctx, msg)
-	return err
-}
-
-func (h *SemanticDiffHandler) Handle(ctx context.Context, msg SemanticDiffMessage) (pkgwebcap.SemanticDiffResult, error) {
-	if h == nil || h.Service == nil {
-		return pkgwebcap.SemanticDiffResult{}, fmt.Errorf("webcap semantic diff service is not configured")
+	var run func(context.Context, SemanticDiffMessage) (pkgwebcap.SemanticDiffResult, error)
+	if service != nil {
+		run = func(ctx context.Context, msg SemanticDiffMessage) (pkgwebcap.SemanticDiffResult, error) {
+			return service.SemanticDiff(ctx, msg.Request)
+		}
 	}
-	return h.Service.SemanticDiff(ctx, msg.Request)
+	return &SemanticDiffHandler{commandHandler: newCommandHandler("semantic diff", run)}
 }
 
 var _ command.Commander[SemanticDiffMessage] = (*SemanticDiffHandler)(nil)
